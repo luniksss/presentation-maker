59978fd1c61f68108cf68d68a0bbefc22cc16296
luniksss <nikolaeval316@gmail.com>
2025-01-15 14:57:04 +0300

    у экспорта json имя презентации

--- src/store/exportPresentationData.tsx
+++ src/store/exportPresentationData.tsx
@@ -4,10 +4,11 @@ function exportPresentationData(editor: Editor): Editor {
     const editorData = JSON.stringify(editor)
     const blob = new Blob([editorData], { type: 'application/json' })
     const url = URL.createObjectURL(blob)
-
+    const presentationName = editor.presentation.title
+    
     const a = document.createElement('a')
     a.href = url
-    a.download = "presentation.json"
+    presentationName !== '' ? a.download = `${presentationName}.json` : a.download = "presentation.json"
     document.body.appendChild(a)
     a.click()
     document.body.removeChild(a)
